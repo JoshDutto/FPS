671bef153a51c1c7f9bebc85544494cb8edafece
Josh Dutton <joshdutto@hotmail.com>
2026-03-25 15:30:22 +0000

    Player view movement
    Added view camera movement using the mouse for the player along the y axis

--- characters/player/camera_3d.gd
+++ characters/player/camera_3d.gd
@@ -3,10 +3,16 @@ extends Camera3D
 @onready var camera_3d = $"."
 
 @export var mouse_sensitivity_h = 0.15
-@export var mouse_sensitivity_v = 0.15
+@export var mouse_sensitivity_v = 0.10
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity_h
+		camera_3d.rotation_degrees.x -= event.relative.y * mouse_sensitivity_v
+		camera_3d.rotation_degrees.x = clamp(camera_3d.rotation_degrees.x, -90, 90)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
