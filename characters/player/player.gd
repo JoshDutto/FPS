8ab31b08083a941b355310622dffbbc65525488a
Josh Dutton <48409604+JoshDutto@users.noreply.github.com>
2026-03-26 19:53:21 +0000

    CharacterMover script
    Implemented the character mover script which controls the velocity of the player along the x and z axis with WASD control. Also allows the player to jump.

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var camera_3d = $Camera3D
+@onready var character_mover: Node3D = $CharacterMover
 
 @export var mouse_sensitivity_h = 0.15
 @export var mouse_sensitivity_v = 0.10
@@ -24,3 +25,11 @@ func _process(delta):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var move_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	character_mover.set_move_dir(move_dir)
+	if Input.is_action_just_pressed("jump"):
+		character_mover.jump()
+		#todo
