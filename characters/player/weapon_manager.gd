2791f1ec42df4dd112c0c2d10802d2fc38be6301
Josh Dutton <joshdutto@hotmail.com>
2026-04-07 18:49:43 +0100

    Update weapon_manager.gd
    Update of weapon_manager to enable weapon switching through inventory slots

--- characters/player/weapon_manager.gd
+++ characters/player/weapon_manager.gd
@@ -12,13 +12,34 @@ func _ready ():
 		weapons_unlocked.append(false)
 
 func disable_all_weapons ():
-	pass
+	for weapon in weapons:
+		if has_method("set_active"):
+			weapon.set_active = false
+		else:
+			weapon.hide()
 	
 func switch_to_previous_weapon():
-	pass
+	for i in range (weapons.size()):
+		var wrapped_ind = wrapi(cur_slot - 1 -i, 0, weapons.size())
+		if switch_to_weapon_slot(wrapped_ind):
+			break
 	
 func switch_to_next_weapon():
-	pass
+	for i in range (weapons.size()):
+		var wrapped_ind = wrapi(cur_slot + 1 +i, 0, weapons.size())
+		if switch_to_weapon_slot(wrapped_ind):
+			break
 
-func switch_to_weapon_slot(slot_int: int):
-	pass
+func switch_to_weapon_slot(slot_ind: int) -> bool:
+	if slot_ind > weapons.size() -1 or slot_ind < 0: 
+		return false
+	if weapons_unlocked.size() == 0 or !weapons_unlocked[slot_ind]:
+		return false
+	disable_all_weapons()
+	cur_slot = slot_ind
+	cur_weapon = weapons[cur_slot]
+	if has_method("set_active"):
+		cur_weapon.set_active = true
+	else:
+		cur_weapon.show()
+	return true
